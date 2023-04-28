b325c907146075ea29db246db271d5f3c788a1af
Daniel Garon <dgaron@colostate.edu>
2023-04-27 18:08:41 -0600

    Restore S macros

--- MiniMFE/MiniMFE.c
+++ MiniMFE/MiniMFE.c
@@ -113,8 +113,7 @@ float reduce_MiniMFE_T_1(long, int, int, float**);
 #define B(i) B[i]
 #define W(i,j) W[i][j]
 #define T(i,j) T[i][j]
-#define H(i,j) H[MOD((i+j),(N-1))]
-#define BLOCK_SIZE 64
+#define H(i,j) H[MOD(i + j, N + 1)]
 
 void MiniMFE(long N, float* A, float* B, float** W, float* score){
 	///Parameter checking
@@ -133,24 +132,15 @@ void MiniMFE(long N, float* A, float* B, float** W, float* score){
 		T[mz1] = &_lin_T[(mz1*(N+1))];
 	}
 	
-	float* H = (float*)malloc(sizeof(float)*(N-1));
+	float* H = (float*)malloc(sizeof(float*)*(N+1));
 	mallocCheck(H, (N+1), float*);
 
-	#define S2(i,j) H(i,j) = foo(A(i),B(j))
-	#define S3(i,j) H(i,j) = __min_float(foo(A(i),B(j)),__min_float(H(i+1,j),H(i,j-1)))
-	#define S4(i,j) H(i,j) = bar((foo(A(i),B(j)))+(T(i+1,j-1)),H(i+1,j),H(i,j-1))
-	#define S5(i0,i1) *score = T(0,N)
-	#define S0(i,j) T(i,j) = __min_float(W(i,j),H(i,j))
-	#define S1(i,j) T(i,j) = __min_float(__min_float(H(i,j),W(i,j)),reduce_MiniMFE_T_1(N,i,j,T))
-
-	/*
 	#define S2(i,j) H(-i+N,j) = foo(A(-i+N),B(j))
 	#define S3(i,j) H(-i+N,j) = __min_float(foo(A(-i+N),B(j)),__min_float(H(-i+N+1,j),H(-i+N,j-1)))
 	#define S4(i,j) H(-i+N,j) = bar((foo(A(-i+N),B(j)))+(T(-i+N+1,j-1)),H(-i+N+1,j),H(-i+N,j-1))
 	#define S5(i0,i1) *score = T(0,N)
 	#define S0(i,j) T(-i+N,j) = __min_float(W(-i+N,j),H(-i+N,j))
 	#define S1(i,j) T(-i+N,j) = __min_float(__min_float(H(-i+N,j),W(-i+N,j)),reduce_MiniMFE_T_1(N,-i+N,j,T))
-	*/
 	{
 		//Domain
 		//{i,j|i+j==N && N>=1 && N>=i && i>=0}
@@ -159,42 +149,25 @@ void MiniMFE(long N, float* A, float* B, float** W, float* score){
 		//{i0,i1|i1==N+1 && i0==N+1 && N>=1}
 		//{i,j|i+j==N && N>=1 && N>=i && i>=0}
 		//{i,j|i+j>=N+1 && N>=1 && N>=i && N>=j && i+j>=1}
-		int c1, c2, ii, jj;
-		S2((N),(N));
-		S0((N),(N));
-		S2((N-1),(N-1));
-		S0((N-1),(N-1));
-		S3((N-1),(N));
-		S1((N-1),(N));
-
-		for (ii = N-2; ii >= 0; ii -= BLOCK_SIZE) {
-			for (jj = ii; jj <= N-2; jj += (BLOCK_SIZE*1)) {
-
-				for(c1=ii; c1 >= max(ii - BLOCK_SIZE, 0); c1-=1){
-					S2((c1),(c1));
-					S0((c1),(c1));
-					S3((c1),(c1+1));
-					S1((c1),(c1+1));
-					for(c2=c1; c2 <= min(jj + (BLOCK_SIZE*1), N-2); c2+=1){
-						S4((c1),(c2+2));
-						S1((c1),(c2+2));
-					}
-				}
-
-			}
-		}
-
-		/*for(c1=N-2; c1 >= 0; c1-=1){
-		 	S2((c1),(c1));
-		 	S0((c1),(c1));
-		 	S3((c1),(c1+1));
-		 	S1((c1),(c1+1));
-		 	for(c2=c1; c2 <= N-2; c2+=1){
-		 	 	S4((c1),(c2+2));
-		 	 	S1((c1),(c2+2));
-		 	}
-		}*/
-
+		int c1,c2;
+		S2((0),(N));
+		S0((0),(N));
+		S2((1),(N-1));
+		S0((1),(N-1));
+		S3((1),(N));
+		S1((1),(N));
+		for(c1=2;c1 <= N;c1+=1)
+		 {
+		 	S2((c1),(-c1+N));
+		 	S0((c1),(-c1+N));
+		 	S3((c1),(-c1+N+1));
+		 	S1((c1),(-c1+N+1));
+		 	for(c2=-c1+N+2;c2 <= N;c2+=1)
+		 	 {
+		 	 	S4((c1),(c2));
+		 	 	S1((c1),(c2));
+		 	 }
+		 }
 		S5((N+1),(N+1));
 	}
 	#undef S2
@@ -217,9 +190,10 @@ float reduce_MiniMFE_T_1(long N, int ip, int jp, float** T){
 		//Domain
 		//{i,j,k|jp>=ip+1 && N>=jp && ip>=0 && N>=1 && N+jp>=ip+1 && j>=k+1 && N+j>=i+1 && i>=0 && k>=i && N>=k && N>=j && k>=-1 && j>=i+1 && ip==i && jp==j}
 		int c3;
-		for(c3=ip; c3 <= jp-1; c3+=1){
+		for(c3=ip;c3 <= jp-1;c3+=1)
+		 {
 		 	S2((ip),(jp),(c3));
-		}
+		 }
 	}
 	#undef S2
 	return reduceVar;
